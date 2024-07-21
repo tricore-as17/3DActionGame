738dc2945bd6a143ae66773fc2d1bb38bf972481
tricore.as17 <tricore.as17@gmail.com>
2024-07-21 12:41:58 +0900

    GameとMainの処理の追加

---
+++ GameProject/GameProject/InputManager.cpp
@@ -0,0 +1,93 @@
+﻿#include"DxLib.h"
+#include"InputManager.h"
+
+
+//nullポインターを入れる
+InputManager* InputManager::inputManager = nullptr;
+//フラグの初期化
+bool InputManager::releaseKey = false;
+bool InputManager::onKey = false;
+bool InputManager::onPrevKey = false;
+
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+InputManager::InputManager()
+{
+    //処理なし
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+InputManager::~InputManager()
+{
+    //処理なし
+}
+
+/// <summary>
+/// インスタンスを作成する
+/// </summary>
+void InputManager::CreateInstance()
+{
+    //既にインスタンスが確保されているかのチェック
+    if (inputManager != nullptr)
+    {
+        return;
+    }
+
+    //シングルトンの作成
+    inputManager = new InputManager();
+}
+
+
+/// <summary>
+/// インスタンスの削除
+/// </summary>
+void InputManager::DeleteInstance()
+{
+    if (inputManager)
+    {
+        delete inputManager;
+    }
+    inputManager = nullptr;
+}
+
+/// <summary>
+/// キーを離した瞬間をとる
+/// </summary>
+/// <param name="compareKey">チェックしたい入力キー</param>
+/// <returns>キーを離したか</returns>
+bool InputManager::IsReleaseKey(const int compareKey)
+{
+    //キー入力
+    auto input = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+
+
+    releaseKey = false;
+    // キー離した瞬間を取る.
+    if (onKey)
+    {
+        //
+        if (!(input & PAD_INPUT_10))
+        {
+            onKey = false;
+            releaseKey = true;
+        }
+    }
+    else if (onPrevKey == false && (input & PAD_INPUT_10))	//キーが最初に押されたタイミング
+    {
+        releaseKey = false;
+        onKey = true;
+    }
+    if ((input & PAD_INPUT_10))	//押し続けられている時の処理
+    {
+        onPrevKey = true;
+    }
+    else
+    {
+        onPrevKey = false;
+    }
+    return releaseKey;
+}
--- GameProject/GameProject/TitleScene.cpp
+++ GameProject/GameProject/TitleScene.cpp
@@ -1,1 +1,35 @@
-﻿
+﻿#include"TitleScene.h"
+
+
+/// <summary>
+/// コンストラクタ
+/// </summary>
+TitleScene::TitleScene()
+{
+
+}
+
+/// <summary>
+/// デストラクタ
+/// </summary>
+TitleScene::~TitleScene()
+{
+
+}
+
+/// <summary>
+/// 更新処理
+/// </summary>
+void TitleScene::Update()
+{
+
+}
+
+
+/// <summary>
+/// 描画
+/// </summary>
+void TitleScene::Draw()
+{
+
+}
