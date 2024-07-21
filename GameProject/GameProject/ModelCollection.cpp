6a048dc84e2cb177a5b8a80d412ece52cbf44bcb
tricore.as17 <tricore.as17@gmail.com>
2024-07-21 19:33:28 +0900

    モデルコレクションの処理の追加

--- GameProject/GameProject/ModelCollection.cpp
+++ GameProject/GameProject/ModelCollection.cpp
@@ -1,10 +1,18 @@
-﻿#include"ModelCollection.h"
+﻿#include"DxLib.h"
+#include"ModelCollection.h"
+
+//自身の「ポインタをNULLにしておく
+ModelCollection* ModelCollection::modelCollection = nullptr;
 
 /// <summary>
 /// コンストラクタ
 /// </summary>
 ModelCollection::ModelCollection()
 {
+    //モデルのロード
+    modelHandle.insert(make_pair(Player, MV1LoadModel("Model/playerPico.mv1")));
+    modelHandle.insert(make_pair(Boss, MV1LoadModel("Model/BOSS.mv1")));
+    modelHandle.insert(make_pair(Stage, MV1LoadModel("Model/map2.mv1")));
 
 }
 
@@ -13,7 +21,11 @@ ModelCollection::ModelCollection()
 /// </summary>
 ModelCollection::~ModelCollection()
 {
-
+    //モデルのアンロード
+    for (int i = 0; i < modelHandle.size(); i++)
+    {
+        MV1DeleteModel(modelHandle.at((ModelTag)(i)));
+    }
 }
 
 /// <summary>
@@ -21,7 +33,11 @@ ModelCollection::~ModelCollection()
 /// </summary>
 void ModelCollection::CreateInstance()
 {
-
+    //インスタンスが作られていなければ作成
+    if (modelCollection == nullptr)
+    {
+        modelCollection = new ModelCollection();
+    }
 }
 
 /// <summary>
@@ -29,7 +45,10 @@ void ModelCollection::CreateInstance()
 /// </summary>
 void ModelCollection::DeleteInstance()
 {
-
+    if (modelCollection != nullptr)
+    {
+        delete modelCollection;
+    }
 }
 
 /// <summary>
@@ -39,6 +58,5 @@ void ModelCollection::DeleteInstance()
 /// <returns>イメージハンドル</returns>
 const int ModelCollection::GetModelHandle(ModelTag modelTag)
 {
-    int modelHandle = 0;
-    return modelHandle;
+    return modelHandle.at(modelTag);
 }
