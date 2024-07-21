1876fdb5ab232243877eb36eb753966c2528de2d
tricore.as17 <tricore.as17@gmail.com>
2024-07-21 19:33:28 +0900

    モデルコレクションの追加

--- GameProject/GameProject/ModelDataManager.cpp
+++ GameProject/GameProject/ModelDataManager.cpp
@@ -1,10 +1,16 @@
 ﻿#include"ModelDataManager.h"
+#include"DxLib.h"
+
 
 /// <summary>
 /// コンストラクタ
 /// </summary>
 ModelDataManager::ModelDataManager()
 {
+    //モデルのロード
+    modelHandle.insert(make_pair(Player, MV1LoadModel("Model/playerPico.mv1")));
+    modelHandle.insert(make_pair(Boss, MV1LoadModel("Model/BOSS.mv1")));
+    modelHandle.insert(make_pair(Stage, MV1LoadModel("Model/map2.mv1")));
 
 }
 
@@ -13,7 +19,11 @@ ModelDataManager::ModelDataManager()
 /// </summary>
 ModelDataManager::~ModelDataManager()
 {
-
+    //モデルのアンロード
+    for (int i = 0; i < modelHandle.size(); i++)
+    {
+        MV1DeleteModel(modelHandle.at((ModelTag)(i)));
+    }
 }
 
 /// <summary>
@@ -21,7 +31,11 @@ ModelDataManager::~ModelDataManager()
 /// </summary>
 void ModelDataManager::CreateInstance()
 {
-
+    //インスタンスが作られていなければ作成
+    if (modelDataManager == nullptr)
+    {
+        modelDataManager = new ModelDataManager();
+    }
 }
 
 /// <summary>
@@ -29,7 +43,10 @@ void ModelDataManager::CreateInstance()
 /// </summary>
 void ModelDataManager::DeleteInstance()
 {
-
+    if (modelDataManager != nullptr)
+    {
+        delete modelDataManager;
+    }
 }
 
 /// <summary>
@@ -39,9 +56,5 @@ void ModelDataManager::DeleteInstance()
 /// <returns>イメージハンドル</returns>
 const int ModelDataManager::GetModelHandle(ModelTag modelTag)
 {
-    //ToDo
-    //プレイヤーなどを追加した際にそのモデルハンドルを返すため
-    //今はエラーが起きないように0を返しています。
-    int modelHandle = 0;
-    return modelHandle;
+    return modelHandle.at(modelTag);
 }
